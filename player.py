dc18b9d4b9f81b0f9705448ae3c7660c9b78cfca
anhphan <anhphan@anhphans-MacBook-Air.local>
2016-11-27 00:00:44 +1000

    Add in the board game logic and human player

--- player.py
+++ player.py
@@ -1,8 +1,20 @@
+'''
+    Implementation of different tic tac toe AI agent
 
+    @created: 26/11/2016
+    @author: veng.thai@gmail.com
+'''
+
+'''
+    Abstract player class
+'''
 class Player(object):
     def play(self, move):
         pass
 
+'''
+    This is the normal human player
+'''
 class HumanPlayer(Player):
     def __init__(self, gameboard):
         self.gameboard = gameboard
@@ -10,11 +22,18 @@ class HumanPlayer(Player):
     def play(self, move):
         self.gameboard.record(move)
         while True:
-            # move is an integer specifying the index of the cell played
-            m = int(input("Your Move -> "))
-            if m not in self.gameboard.legal_moves():
-                print ("Error: illegal move")
+            # move is a tuple to specify the row and column in the game board
+            row, column = tuple(map(int, raw_input().split(' ')))
+            if (row, column) not in self.gameboard.legal_moves():
+                print ("Error: illegal move, please re-enter the coordinates:"),
                 continue
             break
-        self.gameboard.record(m)
-        return m 
+        self.gameboard.record((row, column))
+        return (row, column)
+
+'''
+    This is the AI agent using Minmax algorithm
+'''
+class Minmax_player(Player):
+    def play(self, move):
+        pass
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,85 +1,106 @@
+'''
+    Implementation of different tic tac toe AI agent
 
-from sets import Set
+    @created: 26/11/2016
+    @author: veng.thai@gmail.com
+'''
 
-import array
+import numpy as np
+
 import player
 
+'''
+    This is the gameboard for the tic tac toe, it record the status of the game move and provides
+    method to determine who wins a game and what are legal moves in a game
+'''
 class TicTacToeBoard(object):
-
     def __init__(self, n):
         self.n = n
-        self.board = array.array('b', [0]*n*n)
+        self.board = np.zeros((3, 3), dtype=int)
         self.symbol = {0: '.', 1: 'x', -1: 'o'}
+        self.name = {1: 'White (x)', -1: 'Black (o)'}
         self.turn = 1;
 
     def display(self):
         print ''
-        for r in range(self.n):
-            print (' ' * 5),
-            for c in range(self.n):
-                print self.symbol[self.board[r * self.n + c]],
-            print ''
+        for row in range(0, self.n):
+            rowStr = [self.symbol[cell] for cell in self.board[row, :]]
+            print '\t' + ' '.join(rowStr)
         print ''
+
+    def current_player_name(self):
+        return self.name[self.turn]
+
+    def previous_player_name(self):
+        return self.name[self.turn * -1]
 
     def clone(self):
         t3 = TicTacToeBoard(self.n)
-        #t3 = elf.board[:]
         return t3
 
-    def i2rc(self,i):
-        '''
-        Convert from i index to row, column
-        '''
-        return  i/self.n, i%self.n
-
-
-    def rc2i(self,r,c):
-        '''
-        Convert from row, column to i index 
-        '''
-        return r*self.n+c
-
     def record(self, move):
-        #print 'recording', move, 'for player', self.turn
         if move is not None:
             self.board[move] = self.turn
             self.turn = self.turn * -1
 
     def legal_moves(self):
-        return [i for i in range(self.n * self.n) if self.board[i] == 0]
+        x, y = np.where(self.board == 0)
+        return zip(x, y)
 
     def game_ends(self, last_move):
         if last_move is None:
             return False
-        # horizontal
-        cr, cc = self.i2rc(last_move)
 
-        lr, lc = self.i2rc(last_move - 2)
+        row = last_move[0]
+        column = last_move[1]
 
-        left = last_move - 2 if lr == cr else 0
-        rr, rc = self.i2rc(last_move + 2)
+        left = max(0, column - 3)
+        right = min(self.n, column + 3)
+        top = max(0, row - 3)
+        bottom = min(self.n, row + 3)
 
-        right = last_move + 2 if rr == cr else self.n * (cr+1) - 1
-        print 'left, right', left, right, last_move
-        sum = 0;
-        for i in range(left, right + 1):
-            sum = sum + self.board[i]
-
-        print 'sum h', sum
-        if sum != 3 and sum != -3:
-            # vertical
-            top = (last_move - self.n)%self.n if (last_move - self.n)%self.n >= 0 else last_move - self.i2rc(last_move)[0]*self.n
-            down = (last_move + self.n * 2) if (last_move + self.n * 2) < self.n * self.n else last_move + (self.n * (self.n - self.i2rc(last_move)[0] - 1))
-            sum = 0
-            for i in range(top, down/self.n + 1):
-                sum = sum + self.board[i * self.n]
-
-        last_move_player = self.turn * -1
-        if last_move_player == 1 and sum == 3:
+        left_to_right = self.board[row, left:right]
+        total_3_cell = left_to_right == 1
+        if total_3_cell.sum() == 3:
             return True
-        elif last_move_player == -1 and sum == -3:
+        
+        total_3_cell = left_to_right == -1
+        if total_3_cell.sum() == 3:
             return True
+
+        top_to_bottom = self.board[top:bottom, column]
+        total_3_cell = top_to_bottom == 1
+        if total_3_cell.sum() == 3:
+            return True
+        
+        total_3_cell = top_to_bottom == -1
+        if total_3_cell.sum() == 3:
+            return True
+
+        diag_row = range(top, bottom)
+        diag_column = range(left, right)
+
+        diag_left = self.board[diag_row, diag_column]
+        total_3_cell = diag_left == 1
+        if total_3_cell.sum() == 3:
+            return True
+        
+        total_3_cell = diag_left == -1
+        if total_3_cell.sum() == 3:
+            return True
+
+        diag_column.reverse()
+        diag_right = self.board[diag_row, diag_column]
+        total_3_cell = diag_right == 1
+        if total_3_cell.sum() == 3:
+            return True
+        
+        total_3_cell = diag_right == -1
+        if total_3_cell.sum() == 3:
+            return True
+
         return False
+
 
 class Game(object):
 
@@ -94,12 +115,16 @@ class Game(object):
 
         while not self.t3_board.game_ends(last_move):
             self.t3_board.display()
-            print 'Player %s to move ' %('Black' if self.t3_board.turn == -1 else 'White')
+
+            print 'Legal moves coordinates:', self.t3_board.legal_moves()
+            print '\nInput a coordinate to move to by specifying the row and column indexes (seperated by a space)'
+            print 'Player %s to move:' %(self.t3_board.current_player_name()),
+            
             move = self.players[self.t3_board.turn].play(last_move)
             self.t3_board.record(move)
             last_move = move
 
-        print 'Game ends'
+        print '\nGame ends with player %s wins' %(self.t3_board.previous_player_name())
         self.t3_board.display()
 
 
